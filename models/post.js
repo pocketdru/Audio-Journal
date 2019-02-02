7493ba0f957b1097da1ceafcd16743e2a68b883a
Mary <pocketdru@gmail.com>
2019-02-02 13:58:43 -0700

    commit1

---
+++ models/post.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, DataType) {
+    var Post = sequelize.define("Post", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: [1]
+        },
+
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        }
+
+    });
+
+    Post.associate = function (models) {
+        Post.belongsTo(models.User, {
+            foreighKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Post;
+};
