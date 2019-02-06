6408d276bdd5b1d2ee7bae0e6d422cb5336e765d
Mary <pocketdru@gmail.com>
2019-02-05 19:38:42 -0700

    ffiiiixxx

--- models/post.js
+++ models/post.js
@@ -1,25 +1,24 @@
-module.exports = function (sequelize, DataType) {
-    var Post = sequelize.define("Post", {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: [1]
-        },
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: [1]
+    },
 
-        body: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            len: [1]
-        }
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    }
+  });
 
+  Post.associate = function(models) {
+    Post.belongsTo(models.User, {
+      foreighKey: {
+        allowNull: false
+      }
     });
-
-    Post.associate = function (models) {
-        Post.belongsTo(models.User, {
-            foreighKey: {
-                allowNull: false
-            }
-        });
-    };
-    return Post;
+  };
+  return Post;
 };
--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: DataTypes.STRING,
     password: DataTypes.TEXT
   });
 
-  User.associate = function (models) {
+  User.associate = function(models) {
     // Associating User with Posts
     User.hasMany(models.Post, {
       onDelete: "cascade"
